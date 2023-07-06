d116926c2b59f157755d93684afd43cb5aaf729d
jac <joao.coutinho@sebasi.pt>
2023-07-06 16:23:49 +0100

    feat: save articles on localstorage

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 import { copy, linkIcon, loader, tick } from '../assets';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 function Demo() {
 
@@ -10,10 +11,33 @@ function Demo() {
     summary: '',
   });
 
+  const [allArticles, setAllArticles] = useState([]);
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage)
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
-    alert('Submited');
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if(data?.summary) {
+      const newArticle = { ...article, summary: data.summary};
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
+    }
   }
-  
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
