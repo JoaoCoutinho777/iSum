df98ec64d92d62b9442d0d6cfee87ba59fb6e1e1
jac <joao.coutinho@sebasi.pt>
2023-07-06 16:24:51 +0100

    Revert "feat: save articles on localstorage"
    This reverts commit d116926c2b59f157755d93684afd43cb5aaf729d.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 
 import { copy, linkIcon, loader, tick } from '../assets';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useLazyGetSummaryQuery } from "../services/article";
 
 function Demo() {
 
@@ -11,33 +10,10 @@ function Demo() {
     summary: '',
   });
 
-  const [allArticles, setAllArticles] = useState([]);
-
-  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
-
-  useEffect(() => {
-    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
-
-    if (articlesFromLocalStorage) {
-      setAllArticles(articlesFromLocalStorage)
-    }
-  }, []);
-
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    const { data } = await getSummary({ articleUrl: article.url });
-
-    if(data?.summary) {
-      const newArticle = { ...article, summary: data.summary};
-      const updatedAllArticles = [newArticle, ...allArticles];
-
-      setArticle(newArticle);
-      setAllArticles(updatedAllArticles);
-
-      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
-    }
+    alert('Submited');
   }
-
+  
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
